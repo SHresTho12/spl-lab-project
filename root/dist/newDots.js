c171d68458882f93b15e71eb07c6d1528a406aff
SHresTho12 <talimul12bari@gmail.com>
2021-08-09 07:02:29 +0600

    nothiong

--- root/dist/newDots.js
+++ root/dist/newDots.js
@@ -15,7 +15,7 @@
   \**************************************************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://root/./scripts/Games/ConnectDotsGame/newDots.js?");
+eval("var data = { //global variable set equal to object\r\n    canvas: null,\r\n    ctx: null,\r\n    clickedDot: null, //dot that was previously clicked--fromDot\r\n    // dots: [{ x: 200, y: 200 }, { x: 300, y: 200 }, { x: 220, y: 150 }, { x: 280, y: 150 }, { x: 250, y: 100 }] // letter A //array-dots variable\r\n        // dots2: [{ x: 100, y: 100 }, { x: 200, y: 100 }, { x: 100, y: 200 }, { x: 200, y: 200 }] // shape rectangle\r\n        dots:null\r\n};\r\n\n\n//# sourceURL=webpack://root/./scripts/Games/ConnectDotsGame/newDots.js?");
 
 /***/ })
 
---
+++ root/scripts/Games/ConnectDotsGame/newDots.js
@@ -0,0 +1,8 @@
+var data = { //global variable set equal to object
+    canvas: null,
+    ctx: null,
+    clickedDot: null, //dot that was previously clicked--fromDot
+    // dots: [{ x: 200, y: 200 }, { x: 300, y: 200 }, { x: 220, y: 150 }, { x: 280, y: 150 }, { x: 250, y: 100 }] // letter A //array-dots variable
+        // dots2: [{ x: 100, y: 100 }, { x: 200, y: 100 }, { x: 100, y: 200 }, { x: 200, y: 200 }] // shape rectangle
+        dots:null
+};
