9d02c473462e95a3bb44039df6c407ec73781552
Tasfia Barshat <hellobars19@gmail.com>
2021-10-04 18:08:15 +0600

    Added Quiz Game

---
+++ root/scripts/quizzes/QuizGame/questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "What is the first letter?",
+        answer: "A",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is this fruit?",
+        answer: "Apple",
+        options: [
+            "Orange",
+            "Mango",
+            "Apple",
+            "Tamarind"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the shape?",
+        answer: "Rectangle",
+        options: [
+            "Square",
+            "Rectangle",
+            "Circle",
+            "Triangle"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the number?",
+        answer: "4",
+        options: [
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the given cartoon character?",
+        answer: "Mickey Mouse",
+        options: [
+            "Miney",
+            "Donald",
+            "Goofey",
+            "Mickey Mouse"
+        ]
+    },
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
