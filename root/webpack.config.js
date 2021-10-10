eef8206fa4379587806fb7eabe5a211119b626fb
SHresTho12 <talimul12bari@gmail.com>
2021-10-11 04:34:57 +0600

    design

--- root/webpack.config.js
+++ root/webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
 		memoryGame: ['./scripts/Games/memoryGame.js'],
 		dots: ['./scripts/Games/ConnectDotsGame/dots.js'],
 		typingTest: ['./scripts/quizzes/typingTest.js'],
-		newDots: ['./scripts/Games/ConnectDotsGame/newDots.js']
+		newDots: ['./scripts/Games/ConnectDotsGame/newDots.js'],
+		login: ['./scripts/LogIn/login.js']
 		
 	  },
 	  output: {
